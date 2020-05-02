487030afab9191cd3b8cff25154f3bf4cfa12d99
Holben888 <holben888@gmail.com>
2020-05-02 13:39:51 -0400

    promisify parts of filesystem calls

--- build.js
+++ build.js
@@ -1,4 +1,5 @@
 const filesystem = require('fs')
+const { promisify } = require('util')
 const { rollup } = require('rollup')
 const livereload = require('livereload')
 const babel = require('rollup-plugin-babel')
@@ -7,19 +8,22 @@ const pageTemplate = require('./routes/pageTemplate')
 
 const liveReloadPort = 35729
 
+const writeFile = promisify(filesystem.writeFile)
+const copyFile = promisify(filesystem.copyFile)
+
 const addLiveReloadScript = {
   name: 'livereload',
-  banner: () => {
-    return `document.write('<script src="http://localhost:${liveReloadPort}/livereload.js?snipver=1"></script>')`
+  outro: () => {
+    return `document.write('<script src="http://localhost:${liveReloadPort}/livereload.js?snipver=1"></script>');`
   },
 }
 
-const consoleLogGreen = text => {
+const consoleLogGreen = (text) => {
   console.log('\u001b[1m\u001b[32m' + text + '\u001b[39m\u001b[22m')
 }
 
 const renderPage = (routeName, useCustomRenderer) =>
-  new Promise(async resolve => {
+  new Promise(async (resolve, reject) => {
     let htmlString = ''
     const routePath = `./routes/${routeName}/`
     if (useCustomRenderer) {
@@ -29,31 +33,39 @@ const renderPage = (routeName, useCustomRenderer) =>
       const markdown = await readMdFile(routePath + 'page.md')
       htmlString = pageTemplate(markdown, routeName)
     }
-    const stream = filesystem.createWriteStream(`public/${routeName}.html`)
-
-    stream.once('open', () => {
-      stream.end(htmlString)
+    try {
+      await Promise.all([
+        writeFile(`public/${routeName}.html`, htmlString),
+        copyFile(routePath + 'styles.css', `public/${routeName}.css`),
+      ])
       resolve()
-    })
+    } catch (err) {
+      reject(err)
+    }
   })
 
-const bundlePage = routeName =>
-  new Promise(async resolve => {
+const bundlePage = (routeName) =>
+  new Promise(async (resolve) => {
+    let plugins = [babel()]
+    if (process.env.MODE === 'dev') {
+      plugins = [...plugins, addLiveReloadScript]
+    }
+
     const bundle = await rollup({
       input: {
         [routeName]: `routes/${routeName}/script.js`,
       },
-      plugins:
-        process.env.MODE === 'dev' ? [addLiveReloadScript, babel()] : [babel()],
+      plugins,
     })
     await bundle.write({
       entryFileNames: '[name].js',
       dir: 'public',
-      format: 'esm',
+      format: 'iife',
     })
     resolve()
   })
 
+// Build script
 ;(() => {
   if (!filesystem.existsSync('public/')) {
     filesystem.mkdirSync('public')
@@ -76,11 +88,14 @@ const bundlePage = routeName =>
             routePath,
             { recursive: true },
             async (event, fileName) => {
+              process.stdout.write('\r\x1b[K')
+              process.stdout.write('Rebuilding...')
               await Promise.all([
                 bundlePage(routeName),
                 renderPage(routeName, hasCustomRenderer),
               ])
-              console.log(`Rebuilt changes to ${fileName}`)
+              process.stdout.write('\r\x1b[K')
+              process.stdout.write(`\r\x1b[KRebuilt changes to ${fileName}`)
             }
           )
         }
