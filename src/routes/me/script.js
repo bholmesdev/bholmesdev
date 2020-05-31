1e03ffdbbfd7c5a99a4ec7eebfb04cc96295a3cb
Holben888 <holben888@gmail.com>
2020-05-31 18:23:39 -0400

    add logic to trigger transitions between random projects 🚀

--- src/routes/me/script.js
+++ src/routes/me/script.js
@@ -1,4 +1,5 @@
 import './styles.scss'
+import projects from '../projectList'
 
 export default () => {
   //colored stripe logic
@@ -41,12 +42,74 @@ export default () => {
   const observer = new IntersectionObserver(observerCallback, observerOptions)
   sectionTargets.forEach((target) => observer.observe(target))
 
-  const clickListener = ({ target }) => {
-    if (target.id === 'generate-random-project') {
-      console.log('cool!')
+  const manageProjects = () => {
+    const firstImgEl = document.getElementById('projects-img-1')
+    const secondImgEl = document.getElementById('projects-img-2')
+    const titleEl = document.getElementById('projects-title')
+    const bodyEl = document.getElementById('projects-body')
+    const diceEl = document.getElementById('dice-container')
+
+    let unusedIndices = []
+    const resetUnusedIndices = () =>
+      (unusedIndices = [...Array(projects.length).keys()])
+    resetUnusedIndices() // set unused indices to list of indices up to projects.length
+    unusedIndices.shift() // remove index 0, since this is the initial index shown
+
+    const newIndex = () => {
+      if (unusedIndices.length === 0) resetUnusedIndices()
+      console.log(unusedIndices)
+
+      const randIndex = Math.floor(Math.random() * unusedIndices.length)
+      return unusedIndices.splice(randIndex, 1)
+    }
+
+    let index = newIndex()
+    secondImgEl.src = projects[index].img.src
+    secondImgEl.alt = projects[index].img.alt
+
+    const setProjectInfo = (nextImgEl, currImgEl) => {
+      titleEl.innerText = projects[index].title
+      bodyEl.innerHTML = projects[index].body
+
+      // wait for dice to hit the computer
+      setTimeout(() => {
+        index = newIndex()
+        nextImgEl.classList.add('visible')
+        currImgEl.classList.remove('visible')
+        // load next image once transition animation finishes
+        setTimeout(() => {
+          currImgEl.src = projects[index].img.src
+          currImgEl.alt = projects[index].img.alt
+        }, 500)
+      }, 400)
+    }
+
+    const triggerDiceRoll = () => {
+      diceEl.classList.remove('animated')
+      window.requestAnimationFrame(() => {
+        window.requestAnimationFrame(() => {
+          diceEl.classList.add('animated')
+        })
+      })
+    }
+
+    let showFirstImg = false
+    return () => {
+      triggerDiceRoll()
+      const nextImgEl = showFirstImg ? firstImgEl : secondImgEl
+      const currImgEl = showFirstImg ? secondImgEl : firstImgEl
+      setProjectInfo(nextImgEl, currImgEl)
+      showFirstImg = !showFirstImg
     }
   }
 
+  const nextProject = manageProjects()
+
+  const clickListener = ({ target }) => {
+    if (target.id === 'generate-random-project') {
+      nextProject()
+    }
+  }
   document.addEventListener('click', clickListener)
 
   // cleanup
