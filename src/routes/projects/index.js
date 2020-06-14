c314a891d49610b005ea593b0dc6e76784d1071a
Holben888 <holben888@gmail.com>
2020-06-14 12:20:52 -0400

    set up project page grid layout

---
+++ src/routes/projects/index.js
@@ -0,0 +1,16 @@
+const pug = require('pug')
+const projects = require('../project-list')
+
+module.exports = () => {
+  const projectsByCategory = projects.reduce((groupedByCategory, project) => {
+    groupedByCategory[project.category] = [
+      ...(groupedByCategory[project.category] || []),
+      project,
+    ]
+    return groupedByCategory
+  }, {})
+
+  return pug.renderFile(__dirname + '/page.pug', {
+    projects: projectsByCategory,
+  })
+}
