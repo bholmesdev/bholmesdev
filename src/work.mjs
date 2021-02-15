5ff8928e4d614d9e8897e872a6310e9ed2e12bdb
Holben888 <bholmesdev@gmail.com>
2021-02-15 14:49:30 -0500

    feat: port works page

---
+++ src/work.mjs
@@ -0,0 +1,46 @@
+export default () => {
+  const slideOutEl = document.getElementById('details-slide-out')
+  if (location.hash) {
+    slideOutEl.className = ''
+  }
+
+  const clickListener = (event) => {
+    const { target } = event
+    if (
+      target.tagName === 'A' &&
+      target.origin === location.origin &&
+      target.hash
+    ) {
+      event.preventDefault()
+      slideOutEl.className = ''
+      location.replace(target.href)
+    }
+
+    if (target.id === 'close-btn') {
+      event.preventDefault()
+      slideOutEl.className = 'closed'
+      history.pushState('', document.title, location.pathname)
+    }
+  }
+
+  const sectionIds = ['ongoing-section', 'complete-section']
+
+  // setSectionObserver(sectionIds)
+
+  // lazily load videos once tab is visited
+  setTimeout(() => {
+    const videoEls = document.querySelectorAll('[data-route="work"] video')
+    for (let videoEl of videoEls) {
+      videoEl.load()
+      videoEl.play()
+    }
+  }, 0)
+
+  document.addEventListener('click', clickListener)
+
+  //cleanup
+  return () => {
+    slideOutEl.className = 'closed'
+    document.removeEventListener('click', clickListener)
+  }
+}
