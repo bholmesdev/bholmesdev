4b52801671bb4441c39ad4eb52968712720757dd
Holben888 <bholmesdev@gmail.com>
2021-02-15 14:24:52 -0500

    feat: wire up nav animations on route change

---
+++ src/_layouts/index.mjs
@@ -0,0 +1,55 @@
+export default () => {
+  const navDashedLine = document.getElementById('dashed-line-container')
+  const animDurationMS = 800
+
+  ;(function moveDashedLine() {
+    // only trigger animation once the previous animation is done
+    if (navDashedLine.classList.contains('move')) return
+
+    navDashedLine.classList.add('move')
+    setTimeout(() => {
+      navDashedLine.classList.remove('move')
+    }, animDurationMS)
+  })()
+
+  /*--- handle links and navigation ---*/
+  const primaryNavEl = document.getElementById('primary-nav')
+  const jumpToSectionEl = document.getElementById('jump-to-sections')
+
+  ;(function setActiveNavLink() {
+    const primaryNavLinks = primaryNavEl.querySelectorAll('a')
+    primaryNavLinks.forEach((link) => {
+      if (link.pathname === location.pathname) {
+        link.classList.add('active')
+      } else {
+        link.classList.remove('active')
+      }
+    })
+  })()
+
+  const toggleNavEl = (toggleEl, toggleOffEl) => {
+    if (toggleEl.classList.contains('toggled')) {
+      toggleEl.classList.remove('toggled')
+    } else {
+      toggleEl.classList.add('toggled')
+      toggleOffEl.classList.remove('toggled')
+    }
+  }
+
+  const linkEventListener = (event) => {
+    const { target } = event
+    if (target.id === 'primary-nav-toggle') {
+      toggleNavEl(primaryNavEl, jumpToSectionEl)
+    }
+    if (target.id === 'jump-to-section-toggle') {
+      toggleNavEl(jumpToSectionEl, primaryNavEl)
+    }
+  }
+  document.addEventListener('click', linkEventListener)
+
+  return () => {
+    document.removeEventListener('click', linkEventListener)
+    primaryNavEl.classList.remove('toggled')
+    jumpToSectionEl.classList.remove('toggled')
+  }
+}
