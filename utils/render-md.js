c674b65c530513f0c9a09097d231a94f5d7f7305
Holben888 <bholmesdev@gmail.com>
2021-03-23 21:41:29 -0500

    feat: add IDs to all headers

--- utils/render-md.js
+++ utils/render-md.js
@@ -1,9 +1,23 @@
 const md = require('markdown-it')()
 const attrs = require('markdown-it-attrs')
 const prism = require('markdown-it-prism')
+const headingAnchors = require('markdown-it-anchor')
 const container = require('markdown-it-container')
 
 module.exports = (filePath) => {
-  md.use(prism).use(attrs).use(container, 'highlight')
+  md.use(prism)
+    .use(attrs)
+    .use(container, 'highlight')
+    .use(headingAnchors, {
+      slugify: (s) =>
+        'link-' +
+        s
+          .toLowerCase()
+          .replace(/\s+/g, '-') // Replace spaces with -
+          .replace(/[^\w\-]+/g, '') // Remove all non-word chars
+          .replace(/\-\-+/g, '-') // Replace multiple - with single -
+          .replace(/^-+/, '') // Trim - from start of text
+          .replace(/-+$/, ''),
+    })
   return md.render(filePath)
 }
