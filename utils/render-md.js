980d017848b3317becb98d79a903f8b9e18b34eb
Holben888 <bholmesdev@gmail.com>
2021-03-29 11:31:39 -0500

    feat: support Twitter and Codepen embeds on blog posts

--- utils/render-md.js
+++ utils/render-md.js
@@ -1,13 +1,39 @@
-const md = require('markdown-it')()
+const md = require('markdown-it')({ html: true })
 const attrs = require('markdown-it-attrs')
 const prism = require('markdown-it-prism')
 const headingAnchors = require('markdown-it-anchor')
-const container = require('markdown-it-container')
+
+const toCodepenEmbed = (url = '', params = '') => {
+  const defaultTabs = params.replace('default-tab=', '')
+  const [_, slug] = url.match(/\/pen\/([\w|\d]*)$/)
+  const height = 400
+  return `<p class="codepen" data-height="${height}" data-theme-id="light" data-default-tab="${defaultTabs}" data-user="bholmesdev" data-slug-hash="${slug}" style="height: ${height}px; text-align: center" data-pen-title="Detect current section">
+  <span><a href="${url}">See the Pen</a> by Benjamin Holmes (<a href="https://codepen.io/bholmesdev">@bholmesdev</a>)
+  on <a href="https://codepen.io">CodePen</a>.</span>
+</p>
+`
+}
+
+const formatCodepenEmbeds = (rawMarkdown = '') => {
+  const embeds = rawMarkdown.match(/{% codepen (.*) (.*) %}/g)
+  if (!embeds) return rawMarkdown
+
+  let markdown = rawMarkdown
+
+  for (const embed of embeds) {
+    try {
+      const [_, url, params] = embed.match(/{% codepen (.*) (.*) %}/)
+      markdown = markdown.replace(embed, toCodepenEmbed(url, params))
+    } catch (e) {
+      console.error('There was a problem processing a Codepen embed', e)
+    }
+  }
+  return markdown
+}
 
 module.exports = (filePath) => {
   md.use(prism)
     .use(attrs)
-    .use(container, 'highlight')
     .use(headingAnchors, {
       slugify: (s) =>
         'link-' +
@@ -19,5 +45,6 @@ module.exports = (filePath) => {
           .replace(/^-+/, '') // Trim - from start of text
           .replace(/-+$/, ''),
     })
-  return md.render(filePath)
+
+  return md.render(formatCodepenEmbeds(filePath))
 }
