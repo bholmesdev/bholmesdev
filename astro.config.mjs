118d746fff66218d7f88dc16e8f9a2b7749ac372
bholmesdev <bholmesdev@gmail.com>
2022-09-03 12:37:04 -0400

    lint: astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,7 @@
 import { defineConfig } from 'astro/config';
-
-import preact from "@astrojs/preact";
+import preact from '@astrojs/preact';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [preact()]
+  integrations: [preact()],
 });
