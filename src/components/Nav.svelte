0a5423e6206bfa5bedc7dc8cfc0ceeabe3137e48
bholmesdev <bholmesdev@gmail.com>
2023-08-05 15:41:59 -0400

    feat: animate nav bar on scroll

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -1,26 +1,32 @@
 <script>
-    let mainToggled = true;
+    let isNavOpen = true;
+    let windowScroll = 0;
+
+    $: isNavOpen = windowScroll === 0;
 </script>
 
-<nav>
-    <button>TOC</button>
-    <ul id="nav__link-list" class:mainToggled class="links">
+<svelte:window bind:scrollY={windowScroll} />
+
+<nav class:isNavOpen>
+    <!--<button>TOC</button>-->
+    <ul id="nav__link-list" class="links">
         <li><a href="/">Ben</a></li>
         <li><a href="/blog">Blog</a></li>
         <li><a href="/talks">Talks</a></li>
         <li><a href="/videos">Videos</a></li>
     </ul>
-    <button
-        on:click={() => (mainToggled = !mainToggled)}
-        class:mainToggled
-        aria-expanded={mainToggled}
-        aria-controls="nav__link-list"
-        class="toggle"
-    >
-        <div class="stripe stripe-top" />
-        <div class="stripe stripe-middle" />
-        <div class="stripe stripe-bottom" />
-    </button>
+    <div class="toggle-bg">
+        <button
+            on:click={() => (isNavOpen = !isNavOpen)}
+            aria-expanded={isNavOpen}
+            aria-controls="nav__link-list"
+            class="toggle"
+        >
+            <div class="stripe stripe-top" />
+            <div class="stripe stripe-middle" />
+            <div class="stripe stripe-bottom" />
+        </button>
+    </div>
 </nav>
 
 <style lang="scss">
@@ -30,8 +36,24 @@
         display: flex;
         flex-direction: column;
         justify-content: space-between;
+        position: relative;
+    }
+    .toggle::before {
+        content: "";
+        position: absolute;
+        inset: -0.5rem;
+        z-index: -1;
+        border-radius: 0.7rem;
+        background-color: var(--bg-blur);
     }
 
+    .stripe {
+        height: 4px;
+        width: inherit;
+        background-color: currentColor;
+        transition: transform 0.2s ease-in-out;
+        border-radius: 4px;
+    }
     .stripe.stripe-middle {
         transform-origin: center center;
         transition-property: transform, opacity;
@@ -42,14 +64,14 @@
     .stripe.stripe-bottom {
         transform-origin: bottom right;
     }
-    .mainToggled > .stripe-top {
+    .isNavOpen .stripe-top {
         transform: rotate(-45deg);
     }
-    .mainToggled > .stripe-middle {
+    .isNavOpen .stripe-middle {
         opacity: 0;
         transform: scaleX(0);
     }
-    .mainToggled > .stripe-bottom {
+    .isNavOpen .stripe-bottom {
         transform: rotate(45deg);
     }
 
@@ -58,25 +80,20 @@
             opacity: 0;
             visibility: hidden;
             transform: translateX(17em - ($i * 3.5em));
+            transition-delay: (4 - $i) * 50ms;
             pointer-events: none;
         }
     }
     li {
         transition: all 0.2s ease-in-out;
     }
-    .mainToggled > li {
+    .isNavOpen li {
         opacity: 1;
         visibility: visible;
         transform: translateX(0);
         pointer-events: initial;
     }
 
-    .stripe {
-        height: 4px;
-        width: inherit;
-        background-color: currentColor;
-        transition: transform 0.2s ease-in-out;
-    }
     nav {
         position: sticky;
         top: 0;
@@ -88,6 +105,10 @@
         font-size: var(--font-size-md);
         padding-inline: 1rem;
         padding-block: 1rem;
+        transition: background-color 0.2s ease-in-out;
+    }
+    nav.isNavOpen {
+        background-color: var(--bg);
     }
     ul {
         list-style: none;
