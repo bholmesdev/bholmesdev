6098032442fcafc555d8b898022429fd71ddf7d6
bholmesdev <hey@bholmes.dev>
2023-11-12 12:16:23 -0500

    deps: alpine

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,17 +4,19 @@ import { simpleScope } from './vite-plugin-simple-scoped.mjs';
 import checker from 'vite-plugin-checker';
 import markdoc from "@astrojs/markdoc";
 
+import alpinejs from "@astrojs/alpinejs";
+
 // https://astro.build/config
 export default defineConfig({
-    integrations: [svelte(), markdoc()],
-    vite: {
-        plugins: [
-            simpleScope(),
-            checker({
-                typescript: true,
-                overlay: { initialIsOpen: false, badgeStyle: 'left: 55px; bottom: 8px;' },
-                enableBuild: false, // we already check that in `yarn ci:check`
-            }),
-        ]
-    }
+  integrations: [svelte(), markdoc(), alpinejs()],
+  vite: {
+    plugins: [simpleScope(), checker({
+      typescript: true,
+      overlay: {
+        initialIsOpen: false,
+        badgeStyle: 'left: 55px; bottom: 8px;'
+      },
+      enableBuild: false // we already check that in `yarn ci:check`
+    })]
+  }
 });
