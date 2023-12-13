f91d80b0afbeb803abf316a1448fa21fe45e1a28
bholmesdev <hey@bholmes.dev>
2023-12-13 09:58:39 -0500

    deps: remove vite-plugin-checker

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 import { defineConfig } from "astro/config";
 import svelte from "@astrojs/svelte";
 import simpleScope from "vite-plugin-simple-scope";
-import checker from "vite-plugin-checker";
 import markdoc from "@astrojs/markdoc";
 
 import alpinejs from "@astrojs/alpinejs";
@@ -10,16 +9,6 @@ import alpinejs from "@astrojs/alpinejs";
 export default defineConfig({
   integrations: [svelte(), markdoc(), alpinejs()],
   vite: {
-    plugins: [
-      simpleScope(),
-      checker({
-        typescript: true,
-        overlay: {
-          initialIsOpen: false,
-          badgeStyle: "left: 55px; bottom: 8px;",
-        },
-        enableBuild: false, // we already check that in `yarn ci:check`
-      }),
-    ],
+    plugins: [simpleScope()],
   },
 });
