1776a99aa3ebe0bef4696a6b019136dabde9b091
bholmesdev <hey@bholmes.dev>
2023-12-27 11:38:40 -0500

    chore: tailwind config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,12 @@
 import { defineConfig } from "astro/config";
 import simpleScope from "vite-plugin-simple-scope";
 import markdoc from "@astrojs/markdoc";
-
 import alpinejs from "@astrojs/alpinejs";
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [markdoc(), alpinejs()],
+  integrations: [markdoc(), alpinejs(), tailwind()],
   vite: {
     plugins: [simpleScope()],
   },
---
+++ tailwind.config.cjs
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        heading: "var(--font-heading)",
+        body: "var(--font-body)",
+      },
+      textColor: {
+        accent: "var(--accent)",
+      },
+      backgroundColor: {
+        accent: "var(--accent)",
+      },
+      fontSize: {
+        sm: "var(--font-size-sm)",
+        base: "var(--font-size-base)",
+        lg: "var(--font-size-lg)",
+        xl: "var(--font-size-xl)",
+        "2xl": "var(--font-size-2xl)",
+        "3xl": "var(--font-size-3xl)",
+        "4xl": "var(--font-size-4xl)",
+      },
+    },
+  },
+  plugins: [require("@tailwindcss/typography")],
+};
