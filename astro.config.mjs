b37da853701ad733528663aa74614bd182661115
bholmesdev <hey@bholmes.dev>
2024-02-12 18:32:57 -0500

    deps: remove alpine

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,11 @@
 import { defineConfig } from "astro/config";
 import simpleScope from "vite-plugin-simple-scope";
 import markdoc from "@astrojs/markdoc";
-import alpinejs from "@astrojs/alpinejs";
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [markdoc(), alpinejs(), tailwind()],
+  integrations: [markdoc(), tailwind()],
   vite: {
     plugins: [simpleScope()],
   },
