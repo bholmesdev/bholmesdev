7b807ef59c730e8a0b0d779a83b1a08792f75d2d
bholmesdev <hey@bholmes.dev>
2024-02-23 07:58:52 -0500

    config: astro icon

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,13 +3,15 @@ import simpleScope from "vite-plugin-simple-scope";
 import markdoc from "@astrojs/markdoc";
 import tailwind from "@astrojs/tailwind";
 
+import icon from "astro-icon";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [markdoc(), tailwind()],
+  integrations: [markdoc(), tailwind(), icon()],
   vite: {
     plugins: [simpleScope()],
     esbuild: {
-      keepNames: true,
-    },
-  },
+      keepNames: true
+    }
+  }
 });
