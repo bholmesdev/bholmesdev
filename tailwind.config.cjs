326271e8696513cab55f625349a5ea6ff6f779aa
bholmesdev <bholmesdev@gmail.com>
2024-03-16 11:49:52 -0400

    feat: tailwind v4!

---
+++ tailwind.config.cjs
@@ -0,0 +1,7 @@
+/**
+ * File kept for tailwind VS Code extension
+ * @type {import('tailwindcss').Config}
+ */
+module.exports = {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+};
