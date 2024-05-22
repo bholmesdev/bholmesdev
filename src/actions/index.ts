1f95d7000c6f08c37df90bd78e8b9e3a2e1ac733
bholmesdev <hey@bholmes.dev>
2024-05-22 16:16:54 -0400

    feat: newsletter with buttondown

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -4,7 +4,35 @@ import { Ratelimit } from "@upstash/ratelimit";
 import { getLikes, getVisitorId } from "~/utils.server";
 import { getEntry } from "astro:content";
 
+const BUTTONDOWN_URL = "https://api.buttondown.email/v1/";
+
 export const server = {
+  subscribeToNewsletter: defineAction({
+    accept: "form",
+    input: z.object({
+      email: z.string().email(),
+      musicRecs: z.boolean(),
+    }),
+    handler: async ({ email, musicRecs }) => {
+      const ctx = getApiContext();
+      const res = await fetch(new URL("subscribers", BUTTONDOWN_URL), {
+        method: "POST",
+        body: JSON.stringify({ email, tags: musicRecs ? ["music"] : [] }),
+        headers: {
+          "content-type": "application/json",
+          Authorization: `Token ${ctx.locals.runtime.env.BUTTONDOWN_API_KEY}`,
+        },
+      });
+
+      if (!res.ok) {
+        throw new ActionError({
+          code: "BAD_REQUEST",
+          message: "Unable to subscribe to newsletter.",
+        });
+      }
+      return { success: true };
+    },
+  }),
   like: defineAction({
     input: z.object({
       postSlug: z
