5cdbfa0948dc9bddd810b2b9e415a58316f16a98
bholmesdev <hey@bholmes.dev>
2024-05-26 10:34:38 -0400

    Revert "wip: video"
    This reverts commit 213c0730474ea5f201bd19aad8c62804eb9c9691.

--- src/utils.server.ts
+++ src/utils.server.ts
@@ -10,7 +10,24 @@ export const BUTTONDOWN_URL = "https://api.buttondown.email/v1/";
 export async function checkIfRateLimited(
   ctx: Pick<APIContext, "request" | "locals">
 ): Promise<boolean> {
-  return true;
+  const ip = import.meta.env.DEV
+    ? scope("development")
+    : ctx.request.headers.get("cf-connecting-ip");
+  if (!ip) {
+    throw new ActionError({
+      code: "FORBIDDEN",
+      message: "No header found for rate limiting.",
+    });
+  }
+
+  const redis = Redis.fromEnv(ctx.locals.runtime.env);
+  const ipHash = createHash("sha256").update(ip).digest("hex");
+  const ratelimit = new Ratelimit({
+    redis,
+    limiter: Ratelimit.slidingWindow(100, "1 d"),
+  });
+  const limited = await ratelimit.limit(ipHash);
+  return !limited.success;
 }
 
 export async function updateLikes({
@@ -22,7 +39,15 @@ export async function updateLikes({
   liked: boolean;
   ctx: Pick<APIContext, "locals">;
 }): Promise<{ likes: number }> {
-  return { likes: 0 };
+  const redis = Redis.fromEnv(ctx.locals.runtime.env);
+  const likes = await getLikes({ postSlug, ctx });
+  if (liked) {
+    return { likes: await redis.incr(`likes:${postSlug}`) };
+  }
+  if (likes > 0) {
+    return { likes: await redis.decr(`likes:${postSlug}`) };
+  }
+  return { likes };
 }
 
 export async function getLikes({
@@ -32,5 +57,12 @@ export async function getLikes({
   postSlug: string;
   ctx: Pick<APIContext, "locals">;
 }): Promise<number> {
-  return 0;
+  const redis = Redis.fromEnv(ctx.locals.runtime.env);
+  const likesStr = await redis.get(`likes:${postSlug}`);
+  if (!likesStr) return 0;
+
+  const num = Number(likesStr);
+  if (isNaN(num)) return 0;
+
+  return num;
 }
