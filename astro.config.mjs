10cbc4e8d0ed56a0aeeb9fcc48bed8b88eb8d412
bholmesdev <bholmesdev@gmail.com>
2024-06-29 12:20:56 -0400

    deps: switch to netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,21 +2,13 @@ import { defineConfig } from "astro/config";
 import simpleScope from "vite-plugin-simple-scope";
 import markdoc from "@astrojs/markdoc";
 import icon from "astro-icon";
-import cloudflare from "@astrojs/cloudflare";
 import react from "@astrojs/react";
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
   output: "hybrid",
-  adapter: cloudflare({
-    imageService: "custom",
-    platformProxy: {
-      enabled: true,
-    },
-  }),
-  image: {
-    endpoint: import.meta.env.PROD ? "./src/image-passthrough.ts" : undefined,
-  },
+  adapter: netlify(),
   integrations: [
     markdoc({
       allowHTML: true,
@@ -28,9 +20,6 @@ export default defineConfig({
     plugins: [simpleScope()],
     esbuild: {
       keepNames: true,
-    },
-    ssr: {
-      external: ["node:async_hooks", "node:crypto", "perf_hooks", "sharp"],
     },
   },
   experimental: {
